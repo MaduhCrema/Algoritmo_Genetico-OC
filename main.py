9ac9c1253ef9e73296540a0d15735c95e379c04a
Vinicius Muller de Freitas <viniciusmullerdefreitas309@gmail.com>
2022-06-20 14:36:34 -0300

    Criando Struct

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from bin2float import calcBin2Float
 from population import calcXBin
 from population import calcYBin
 from genetic import cruzamento
+from dataclasses import dataclass
 
 #Calculo da função
 def calc(a, b):
     fun = (math.sin(a)**3 * math.sin(b))/(a**3 * (a+b))
     return fun
 
+@dataclass
+class Individuo_X:
+    x: list
+    fitness: float
+
 #Criando as populações
 PopX = []
-for i in range(100):
-    PopX.append(calcXBin())
+for i in range(10):
+    PopX.append(Individuo_X(calcXBin(), 0))
 
-PopY = []
-for i in range(100):
-    PopY.append(calcYBin())
+for i in range(10):
+    PopX[i].fitness = calc(calcBin2Float(PopX[i].x), 0.001)
+    print(PopX[i])
+    print("\n")
